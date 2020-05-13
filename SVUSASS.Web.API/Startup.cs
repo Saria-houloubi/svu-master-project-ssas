b63141d31cc80483654a4a41583b33a5faac853d
Saria Houloubi <houloubis@gmail.com>
2020-05-13 10:56:47 +0400

    Added CORS

--- SVUSASS.Web.API/Startup.cs
+++ SVUSASS.Web.API/Startup.cs
@@ -10,18 +10,45 @@ namespace SVUSASS.Web.API
 {
     public class Startup
     {
+        #region Properties
+        public IConfiguration Configuration { get; }
+        #endregion
+
+        #region Constructer
+        /// <summary>
+        /// Default constructer
+        /// </summary>
+        /// <param name="configuration"></param>
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
+        #endregion
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
             services.AddSingleton<ILoggingService, DefaultLoggingSservice>();
+
+            services.AddCors(options =>
+            {
+                //Add dev CORS Policy
+                options.AddPolicy("Development2", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                           .AllowAnyHeader()
+                           .AllowAnyMethod();
+                });
+                //Addm Production CORS Policy
+                options.AddPolicy("Development", buider =>
+                {
+                    buider.WithOrigins(Configuration.GetSection("CORS_Origins").Get<string[]>())
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -37,6 +64,8 @@ namespace SVUSASS.Web.API
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseCors(env.EnvironmentName);
 
             app.UseHttpsRedirection();
             app.UseMvc();
