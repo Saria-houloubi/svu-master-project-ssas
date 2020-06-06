1eceb56566e8879b600ee56f1583978a4b7cc1ba
Saria Houloubi <houloubis@gmail.com>
2020-06-06 21:13:08 +0400

    Fixed error on relase

--- SVUSASS.Web.API/Startup.cs
+++ SVUSASS.Web.API/Startup.cs
@@ -77,7 +77,8 @@ namespace SVUSASS.Web.API
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-           app.UseMiddleware<CustomLoggingMiddleware>();
+            //REMOVED on 06/06/2020 due to throughing 500 server erros on production
+            //app.UseMiddleware<CustomLoggingMiddleware>();
 
             app.UseCors(env.IsDevelopment() ? "Development" : "Production");
 
